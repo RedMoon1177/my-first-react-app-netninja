9dc7be12843ab48613f138ecc8660769b7e724cc
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-10-27 20:33:02 -0300

    completed Section 7

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./NewEventForm.css";
 
 export default function NewEventForm({ addEvent }) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
+  const [location, setLocation] = useState("manchester");
+
+  // const title = useRef();
+  // const date = useRef();
 
   const resetForm = () => {
     setTitle("");
     setDate("");
+    setLocation("manchester");
+    // title.current.value = "";
+    // date.current.value = "";
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const event = {
+      // title: title.current.value,
+      // date: date.current.value,
       title: title,
       date: date,
+      location: location,
       id: Math.floor(Math.random() * 10000),
     };
 
@@ -29,6 +39,7 @@ export default function NewEventForm({ addEvent }) {
         Event title:
         <input
           type="text"
+          // ref={title}
           onChange={(e) => setTitle(e.target.value)}
           value={title}
         />
@@ -37,9 +48,18 @@ export default function NewEventForm({ addEvent }) {
         <span>Event Date:</span>
         <input
           type="date"
+          // ref={date}
           onChange={(e) => setDate(e.target.value)}
           value={date}
         />
+      </label>
+      <label>
+        <span>Event Location:</span>
+        <select onChange={(e) => setLocation(e.target.value)}>
+          <option value="manchester">Manchester</option>
+          <option value="london">London</option>
+          <option value="cardiff">Cardiff</option>
+        </select>
       </label>
       <button>Submit</button>
     </form>
